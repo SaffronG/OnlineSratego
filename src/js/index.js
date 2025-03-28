a8bba314349df44bdb0b3fa9163f52aadda55945
SaffronGamer <saffrongamer167@gmail.com>
2025-03-28 15:31:29 -0600

    Created a basic grid-pattern

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let message = "Hello, World!";
-console.log(message);
+let board = document.getElementById("game_board");
+buildBoard();
+console.log("Hello World!");
+function buildBoard() {
+    for (let i = 0; i < 100; i++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        board === null || board === void 0 ? void 0 : board.appendChild(cell);
+    }
+}
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,2 +1,12 @@
-let message: String = "Hello, World!"
-console.log(message)
+let board: HTMLElement | null = document.getElementById("game_board");
+
+buildBoard()
+console.log("Hello World!")
+
+function buildBoard() {
+    for (let i = 0; i < 100; i++) {
+        let cell: HTMLDivElement = document.createElement("div");
+        cell.className = "cell";
+        board?.appendChild(cell);
+    }
+}
