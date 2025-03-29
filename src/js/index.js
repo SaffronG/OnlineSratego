baea5064f0fbb74533d3a15bdf156c2cb8c4ade2
SaffronGamer <saffrongamer167@gmail.com>
2025-03-28 19:18:36 -0600

    fixed edge-case event handler add

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 "use strict";
 let board = document.getElementById("game_board");
 buildBoard();
-console.log("Hello World!");
 function buildBoard() {
     for (let i = 0; i < 100; i++) {
         let cell = document.createElement("div");
@@ -9,13 +8,19 @@ function buildBoard() {
         board === null || board === void 0 ? void 0 : board.appendChild(cell);
         if (i > 40 && i < 60) {
             let loc = i % 10;
-            console.log(loc);
             if (loc == 2 || loc == 3 || loc == 6 || loc == 7) {
                 cell.className = "cell_water";
             }
+            else {
+                cell.addEventListener("click", function () {
+                    cell.classList.toggle("active");
+                });
+            }
         }
-        cell.addEventListener("click", function () {
-            cell.classList.toggle("active");
-        });
+        else {
+            cell.addEventListener("click", function () {
+                cell.classList.toggle("active");
+            });
+        }
     }
 }
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,6 @@
 let board: HTMLElement | null = document.getElementById("game_board");
 
 buildBoard()
-console.log("Hello World!")
 
 function buildBoard() {
     for (let i = 0; i < 100; i++) {
@@ -9,14 +8,20 @@ function buildBoard() {
         cell.className = "cell";
         board?.appendChild(cell);
         if (i > 40 && i < 60) {
-            let loc = i % 10;
-            console.log(loc);
+            let loc = i % 10
             if (loc == 2 || loc == 3 || loc == 6 || loc == 7) {
-                cell.className = "cell_water";
+                cell.className = "cell_water"
+            }
+            else {
+                cell.addEventListener("click", function() {
+                    cell.classList.toggle("active");
+                })
             }
         }
-        cell.addEventListener("click", function() {
-            cell.classList.toggle("active");
-        });
+        else {
+            cell.addEventListener("click", function() {
+                cell.classList.toggle("active");
+            })
+        }
     }
 }
